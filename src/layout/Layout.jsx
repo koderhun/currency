f4f371fb8083a962d2dac9ec5062fecd9810fb98
koderhan <rixhun@ya.ru>
2022-11-18 20:37:43 +0600

    onPress append

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,10 +1,14 @@
+import { useEffect } from 'react'
 import { Layout as LayoutAnt } from 'antd'
 import { General } from 'pages/General'
 import { Route, Routes } from 'react-router-dom'
 
-const {Content} = LayoutAnt
+const { Content } = LayoutAnt
 
 export const Layout = () => {
+  useEffect(() => {
+    document.title = 'Converter'
+  }, [])
   return (
     <LayoutAnt className='layout'>
       <Content className='content'>
