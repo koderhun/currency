eed33f23331b7410a949ed5f62b740a9aabee8bf
koderhan <rixhun@ya.ru>
2022-11-18 21:18:46 +0600

    cname

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,4 +1,3 @@
-import { useEffect } from 'react'
 import { Layout as LayoutAnt } from 'antd'
 import { General } from 'pages/General'
 import { Route, Routes } from 'react-router-dom'
@@ -6,9 +5,6 @@ import { Route, Routes } from 'react-router-dom'
 const { Content } = LayoutAnt
 
 export const Layout = () => {
-  useEffect(() => {
-    document.title = 'Converter'
-  }, [])
   return (
     <LayoutAnt className='layout'>
       <Content className='content'>
