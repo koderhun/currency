bd9f066981b481d6656fcf3b6608ba1323d05fde
koderhan <rixhun@ya.ru>
2022-11-21 16:29:53 +0600

    flags

--- src/components/Flags/Flags.jsx
+++ src/components/Flags/Flags.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './styles.scss'
 
 export const Flags = ({name}) => {
-  console.log('process.env.PUBLIC_URL', process.env.PUBLIC_URL)
   return (
     <div className='flag'>
       <img src={`${process.env.PUBLIC_URL}/images/flags/${name}.png`} alt={name} />
