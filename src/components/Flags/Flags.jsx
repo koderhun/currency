56fd1733b4bc24224f79dadcab02e591cca174d1
ramil <>
2023-01-03 15:55:27 +0600

    new flags

--- src/components/Flags/Flags.jsx
+++ src/components/Flags/Flags.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
+import classnames from 'classnames'
 import s from './styles.module.scss'
 
 export const Flags = ({name}) => {
+  const nameISO = name.substring(0, 2).toLowerCase()
+
   return (
-    <div className={s.flag}>
-      <img
-        src={`${process.env.PUBLIC_URL}/images/flags/${name}.png`}
-        alt={name}
-      />
-      <span>{name}</span>
+    <div className={s.flag} title={name}>
+      <span className={classnames('fi', `fi-${nameISO}`, s.flagImg)}></span>
+      <span className={s.name}>{name}</span>
     </div>
   )
 }
