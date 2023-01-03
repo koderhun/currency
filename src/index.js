a155f55401b986815c6515e192c47e0d0ef821c5
ramil <>
2023-01-03 14:06:17 +0600

    manifest

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import {Provider} from 'react-redux'
 import {HashRouter} from 'react-router-dom'
 import {store} from 'store'
+import '/node_modules/flag-icons/css/flag-icons.min.css'
 import './index.scss'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
