e76efdd6dcf854bbfbc7387251ca36a4c1c91c17
Ramil <koderhanov@gmail.com>
2023-04-02 20:07:15 +0600

    init components

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './styles.module.scss'
+
+export const Form = () => {
+  return (
+    <div className={s.form}>Form</div>
+  )
+}
---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import s from './styles.module.scss'
+
+export const Input = () => {
+  return (
+    <div className={s.input}>Input</div>
+  )
+}
