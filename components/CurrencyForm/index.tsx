a7102f722306336a9214a1b081dbfd8c8d64fa78
Ramil <koderhanov@gmail.com>
2023-04-02 09:10:57 +0600

    feat: new settings

---
+++ components/CurrencyForm/index.tsx
@@ -0,0 +1,9 @@
+import s from './CurrencyForm.module.scss'
+
+const CurrencyForm = () => {
+  return (<div className={s.form}>
+    form
+  </div>);
+}
+
+export default CurrencyForm;
---
+++ prebuild.js
@@ -0,0 +1,40 @@
+const fs = require('fs')
+const filePath = './public/meta.json'
+
+const buildNumber = (info) => {
+  const oldVersion = info.version
+  info.version = oldVersion || '1.0.0'
+
+  let versionSplit = info.version.split('.')
+
+  let buildNumber = parseInt(versionSplit.pop())
+  if (isNaN(buildNumber)) {
+    buildNumber = 0
+  }
+
+  versionSplit.push(++buildNumber)
+
+  const version = versionSplit.join('.')
+  console.log(`Up Version: ${oldVersion} => ${version}`)
+  return version
+}
+
+const readJson = JSON.parse(fs.readFileSync(filePath).toString())
+
+const jsonData = {
+  buildDate: new Date().getTime(),
+  version: buildNumber(readJson),
+}
+
+const jsonContent = JSON.stringify(jsonData)
+
+fs.writeFile(filePath, jsonContent, 'utf8', function (error) {
+  if (error) {
+    console.log(
+      'An error occured while saving build date and time to meta.json',
+    )
+    return console.log(error)
+  }
+
+  console.log('Latest build date and time updated in meta.json file')
+})
