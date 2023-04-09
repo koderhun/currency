13da7c4ccc8df5da69e323bafaeabe144b18c6fb
Ramil <koderhanov@gmail.com>
2023-04-09 15:46:58 +0600

    pwa settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,30 +1,15 @@
 import '/node_modules/flag-icons/css/flag-icons.min.css'
 import { Provider } from 'react-redux'
 import { wrapper } from '@/store'
-import Head from 'next/head'
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { HeadHtml } from '@/config/HeadHtml'
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest)
   const { pageProps } = props
   return <>
-    <Head>
-      <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-      <meta
-        name="viewport"
-        content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-      />
-      <meta name="description" content="Currency converter" />
-      <meta name="keywords" content="converter, currency" />
-      <title>Converter</title>
-
-      <link rel="icon" href="./icons/favicon.svg" type="image/svg+xml" />
-      <link rel="apple-touch-icon" href="./icons/favicon.png" />
-      <meta name="theme-color" content="#317EFB" />
-      <link rel="manifest" href="./manifest.json" />
-    </Head>
+    <HeadHtml />
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
