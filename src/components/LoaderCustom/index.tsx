bfc10ca11265aa28730053895251d6a63d390054
Ramil <example@gmail.com>
2023-06-10 06:27:39 +0300

    change loading component

--- src/components/LoaderCustom/index.tsx
+++ src/components/LoaderCustom/index.tsx
@@ -1,7 +1,12 @@
-import { memo } from 'react'
-import s from './styles.module.scss'
+import { memo } from "react";
+import s from "./styles.module.scss";
 
-// eslint-disable-next-line react/display-name
-export const LoaderCustom = memo(() => {
-  return <div className={s.loader}>Loading...</div>
-})
+type TIsLoading = {
+	isLoading: boolean;
+};
+
+export const LoaderCustom = memo(({ isLoading }: TIsLoading) => {
+	console.log("isLoading", isLoading);
+	const loader: string = isLoading ? "Loading..." : "";
+	return <div className={s.loader}>{loader}</div>;
+});
