7a7e39af9a25114907451f7fb51cc7ec21d70197
Ramil <example@gmail.com>
2023-08-06 08:33:16 +0300

    BYN

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,34 +1,38 @@
 export const formInputs = [
-  {
-    code: 'USD',
-    value: ''
-  },
-  {
-    code: 'EUR',
-    value: ''
-  },
-  {
-    code: 'UAH',
-    value: ''
-  },
-  {
-    code: 'KGS',
-    value: ''
-  },
-  {
-    code: 'GEL',
-    value: ''
-  },
-  {
-    code: 'TRY',
-    value: ''
-  },
-  {
-    code: 'RUB',
-    value: ''
-  },
-  {
-    code: 'KZT',
-    value: ''
-  }
-]
+	{
+		code: "USD",
+		value: "",
+	},
+	{
+		code: "EUR",
+		value: "",
+	},
+	{
+		code: "UAH",
+		value: "",
+	},
+	{
+		code: "KGS",
+		value: "",
+	},
+	{
+		code: "GEL",
+		value: "",
+	},
+	{
+		code: "TRY",
+		value: "",
+	},
+	{
+		code: "KZT",
+		value: "",
+	},
+	{
+		code: "RUB",
+		value: "",
+	},
+	{
+		code: "BYN",
+		value: "",
+	},
+];
