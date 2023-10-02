861bd080988208629f6cd2cb1c47b26f75122b57
Ramil <koderhanov@gmail.com>
2023-10-02 10:56:28 +0300

    translate

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
 	return (
-		<Html lang="en" translate="no">
+		<Html lang="en">
 			<Head />
-			<body>
+			<body translate="no">
 				<Main />
 				<NextScript />
 			</body>
