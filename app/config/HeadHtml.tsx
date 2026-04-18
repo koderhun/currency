2e7b3ea935b572c03ece8410fa31efd24584cb92
ramilkhun <koderhanov@gmail.com>
2026-04-18 08:41:00 +0300

    ok

--- app/config/HeadHtml.tsx
+++ app/config/HeadHtml.tsx
@@ -5,17 +5,17 @@ export const HeadHtml = () => {
   return (
     <Head>
       <meta charSet="utf-8" />
-      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta
         name="viewport"
-        content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=5.0"
+        content="viewport-fit=cover, width=device-width, initial-scale=1, maximum-scale=5"
       />
 
       <meta name="description" content="Currency converter" />
       <meta name="keywords" content="converter, currency" />
+      <meta name="application-name" content="Currency" />
       <title>Converter</title>
 
-      <link rel="icon" href="./icons/favicon.png" type="image/x-icon" />
+      <link rel="icon" href="./icons/favicon.png" type="image/png" />
       <link rel="icon" href="./icons/favicon.svg" type="image/svg+xml" />
       <link rel="manifest" href="./manifest.json" />
 
